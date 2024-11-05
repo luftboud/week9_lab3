0ac3b9b04f92f1321664913baae8c065c648f646
Ija Maharyta <ijamaharyta@gmail.com>
2024-11-05 09:32:27 +0200

    wrote doctests

--- history_analysis.py
+++ history_analysis.py
@@ -9,6 +9,25 @@ def sites_on_date(visits: list, date: str)-> set[str]:
     from get_browser_history module)
     :param date: date in format "yyyy-mm-dd"
     :return: set of url visited on date
+    >>> visits = [\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.479048', 374747),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', \
+'2024-08-30', '23:07:00.098770', 43477213508),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-10', '18:13:46.023932', 276719943),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '20:03:11.037561', 72341869425),\
+('https://www.youtube.com/watch?v=lvUyPZShVEY', '(271) Заборонений метод множення. \
+Виключно 18+ - YouTube', '2024-08-10', '18:18:22.740849', 63759076),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '18:19:26.498617', 33561069),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', '2024-08-30', \
+'23:07:00.098770', 0),\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.852762', 11676347),\
+('https://www.mongodb.com/cloud/atlas/register', 'MongoDB Atlas | MongoDB', '2024-08-31', \
+'10:28:28.257917', 5188055),\
+('https://www.calculator.net/binary-calculator.html', 'Binary Calculator', \
+'2024-09-04', '18:48:00.319963', 8814138)\
+]
+    >>> sites_on_date(visits, '2024-08-15')
+    {'https://www.youtube.com/'}
     """
     url_set = set()
     for el in visits:
@@ -25,8 +44,25 @@ def most_frequent_sites(visits: list, number: int)-> set[str]:
     :param visits: all visits in browser history
     :param number: number of most frequent sites to return
     :return: set of most frequent sites
-    >>> visits = get_chrome_os("ija","MacOS")
-    >>> print(most_frequent_sites(visits,5))
+    >>> visits = [\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.479048', 374747),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', \
+'2024-08-30', '23:07:00.098770', 43477213508),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-10', '18:13:46.023932', 276719943),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '20:03:11.037561', 72341869425),\
+('https://www.youtube.com/watch?v=lvUyPZShVEY', '(271) Заборонений метод множення. \
+Виключно 18+ - YouTube', '2024-08-10', '18:18:22.740849', 63759076),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '18:19:26.498617', 33561069),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', '2024-08-30', \
+'23:07:00.098770', 0),\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.852762', 11676347),\
+('https://www.mongodb.com/cloud/atlas/register', 'MongoDB Atlas | MongoDB', '2024-08-31', \
+'10:28:28.257917', 5188055),\
+('https://www.calculator.net/binary-calculator.html', 'Binary Calculator', \
+'2024-09-04', '18:48:00.319963', 8814138)\
+]
+    >>> len(most_frequent_sites(visits,4)) == 4
+    True
     """
     list_of_sites = [el[0] for el in visits]
     set_of_sites = set(list_of_sites)
@@ -39,7 +75,7 @@ def most_frequent_sites(visits: list, number: int)-> set[str]:
         """A func used for sorting according to frequency"""
         return item[0]
     frequency = sorted(frequency, key=determine_frequency, reverse=True)
-    frequency = frequency[:number]
+    frequency = [el for i, el in enumerate(frequency) if i < number]
     return {el[1] for el in frequency}
 
 
@@ -55,6 +91,25 @@ def get_url_info(visits: list, url: str)->tuple:
     :param visits: all visits in browser history
     :param url: url of site to search
     :return: (title, last_visit_date, last_visit_time, num_of_visits, average_time)
+    >>> visits = [\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.479048', 374747),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', \
+'2024-08-30', '23:07:00.098770', 43477213508),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-10', '18:13:46.023932', 276719943),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '20:03:11.037561', 72341869425),\
+('https://www.youtube.com/watch?v=lvUyPZShVEY', '(271) Заборонений метод множення. \
+Виключно 18+ - YouTube', '2024-08-10', '18:18:22.740849', 63759076),\
+('https://www.youtube.com/', '(545) YouTube', '2024-08-15', '18:19:26.498617', 33561069),\
+('http://proslovo.com/story', 'ProSlovo.com / Будинок Слово / Історія', '2024-08-30', \
+'23:07:00.098770', 0),\
+('https://openai.com/chatgpt/', 'ChatGPT | OpenAI', '2024-09-03', '13:43:58.852762', 11676347),\
+('https://www.mongodb.com/cloud/atlas/register', 'MongoDB Atlas | MongoDB', '2024-08-31', \
+'10:28:28.257917', 5188055),\
+('https://www.calculator.net/binary-calculator.html', 'Binary Calculator', \
+'2024-09-04', '18:48:00.319963', 8814138)\
+]
+    >>> get_url_info(visits, 'https://www.youtube.com/')
+    ('(545) YouTube', '2024-08-15', '20:03:11.037561', 3, 24217383479.0)
     """
     title = None
     visit_dates = []
